fdba19899b71d2cb795f399a3259be61730bfe4a
xinyu-jiao00 <xinyujiao2000@gmail.com>
2025-07-01 01:48:07 -0400

    add button
    click button to see my info

---
+++ script.js
@@ -0,0 +1,4 @@
+// Show an introduction message when the button is clicked
+function showIntro() {
+  alert("Hi, I'm Xinyu Jiao. I'm from Beijing, China. This is my first time in New York, and I really love this city!");
+}
